c1189d7764d91b38934e769d67a1c503576d7e16
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-02-01 14:30:54 -0300

    feat: created Router with all routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { DefaultLayout } from "layouts/Default";
+
+import { Home } from "pages/Home";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
