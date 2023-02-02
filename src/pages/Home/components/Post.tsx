050d2ddf7ce241170c158ed59c3a008c4684517f
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-02-02 13:50:59 -0300

    feat: created format to createdAt date issue

--- src/pages/Home/components/Post.tsx
+++ src/pages/Home/components/Post.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-export interface PostProps {
+export interface PostProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   createdAt: string;
   description: string;
 }
 
-export function Post({ createdAt, description, title }: PostProps) {
+export function Post({ createdAt, description, title, ...props }: PostProps) {
+  const publishedDateRelativeToNow = formatDistanceToNow(new Date(createdAt), {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
-    <div className="max-h-[253px] p-8 bg-slate-600 rounded-lg cursor-pointer hover:outline hover:outline-slate-400 transition-all">
+    <button
+      {...props}
+      className="max-h-[253px] p-8 text-left bg-slate-600 rounded-lg cursor-pointer hover:outline hover:outline-slate-400 transition-all"
+    >
       <header className="w-full flex gap-4">
-        <strong className="w-[283px] text-xl text-slate-50 leading-relaxed">
+        <strong className="w-[283px] text-xl text-slate-50 line-clamp-2 leading-relaxed">
           {title}
         </strong>
-        <span className="text-sm text-slate-300">Há 1 dia</span>
+        <span className="text-sm text-slate-300">
+          {publishedDateRelativeToNow}
+        </span>
       </header>
       <p className="mt-5 text-slate-200 line-clamp-4">{description}</p>
-    </div>
+    </button>
   );
 }
